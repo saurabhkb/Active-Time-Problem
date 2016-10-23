7be6113b556c1f9090fa906a47f2cb0d840f1e12
saurabh <me>
2016-10-22 23:55:18 -0400

    style changes to lpsolve

--- lpsolve.py
+++ lpsolve.py
@@ -15,81 +15,49 @@ subject to:
 	x[t, j], y[t] >= 0				for all j in J, t in T
 """
 
-jobs = []
+# read the data file
 with open(sys.argv[1]) as f:
 	g = int(f.readline().strip())
-	for l in f:
-		r, d, p = map(int, l.split())
-		jobs.append((r, d, p))
+	jobs = [map(int, l.split()) for l in f]
+
 
 # obtain parameters
 n = len(jobs)
 T = max([x[1] for x in jobs])
 print "#jobs = ", n, "\n#times = ", T
 
-# create x[t, j] and y[t] variables
-y = [0 for t in range(T)]
-x = [[0 for j in range(n)] for t in range(T)]
 
+# create the model and attempt to solve it
 try:
 	m = Model("LP")
-	for i in range(T):
-		y[i] = m.addVar(vtype=GRB.BINARY, name="y_%d" % i)
-	for i in range(T):
-		for j in range(n):
-			x[i][j] = m.addVar(vtype=GRB.BINARY, name="x_%d_%d" % (i, j))
+	m.setParam('OutputFlag', False)	# quieten model output
+	y = [m.addVar(vtype=GRB.BINARY, name="y_%d" % t) for t in range(T)]
+	x = [[m.addVar(vtype=GRB.BINARY, name="x_%d_%d" % (t, j)) for j in range(n)] for t in range(T)]
 	m.update()
 
 	m.setObjective(sum(y), GRB.MINIMIZE)
 
+	# constraint 1
 	for t in range(T):
 		for j in range(n):
 			m.addConstr(x[t][j] <= y[t], "c1_%d_%d" % (t, j))
+
+	# constraint 2
 	for t in range(T):
 		m.addConstr(sum(x[t]) <= g * y[t], "c2_%d" % t)
+
+	# constraint 3
 	for j in range(n):
 		m.addConstr(sum([x[t][j] for t in range(T)]) >= jobs[j][2], "c3_%d" % j)
-	for t in range(T):
-		for j in range(n):
-			m.addConstr(x[t][j] >= 0, "c4_%d_%d" % (t, j))
+
+	# constraint 4
 	for t in range(T):
 		m.addConstr(y[t] >= 0, "c5_%d" % t)
+		for j in range(n):
+			m.addConstr(x[t][j] >= 0, "c4_%d_%d" % (t, j))
 
 	m.optimize()
+	print "OPT = ", m.objVal
 
-	# for v in m.getVars():
-	# 	print v.varName, v.x
-	print 'Obj:', m.objVal
-
-except GurobiError:
-	print "Error reported"
-
-# try:
-# 	m = Model("mip1")
-# 
-# 	# Create variables
-# 	x = m.addVar(vtype=GRB.BINARY, name="x")
-# 	y = m.addVar(vtype=GRB.BINARY, name="y")
-# 	z = m.addVar(vtype=GRB.BINARY, name="z")
-# 
-# 	# Integrate new variables
-# 	m.update()
-# 
-# 	# Set objective
-# 	m.setObjective(x + y + 2 * z, GRB.MAXIMIZE)
-# 
-# 	# Add constraint: x + 2 y + 3 z <= 4
-# 	m.addConstr(x + 2 * y + 3 * z <= 4, "c0")
-# 
-# 	# Add constraint: x + y >= 1
-# 	m.addConstr(x + y >= 1, "c1")
-# 
-# 	m.optimize()
-# 
-# 	for v in m.getVars():
-# 		print v.varName, v.x
-# 	print 'Obj:', m.objVal
-# except GurobiError:
-# 	print 'Error reported'
-
-
+except GurobiError as e:
+	print "Error reported: ", e
